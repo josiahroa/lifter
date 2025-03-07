51331c33fbc96cf11f9f3688ddde581f32f97fd3
Josiah Roa <siahroa25@gmail.com>
2025-03-07 00:15:20 -0700

    transpile db pkg for nextjs

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // transpilePackages: ["@lifter/db"],
+  transpilePackages: ["@lifter/db"],
 };
 
 export default nextConfig;
